6e4ecce5060c479b62de1ffa54989e5065a5b4d7
Cicero Cordeiro <cicero.cordeiro1995@gmail.com>
2022-12-21 11:17:24 -0300

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,8 @@
+# Desafio 
+frutas = ['Maçã', 'Laranja', 'Morango', 'Limão']
+for indice, fruta in enumerate(frutas, 1):
+    if indice == 3:
+        print(f'{indice} {fruta} EM PROMOÇÃO')
+    else:
+        print(indice, fruta)
+    
